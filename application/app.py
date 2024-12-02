6e06c001b16adc0095fa302bf68b084e05ec361d
sharbeldahlan <sharbel.dahlan@gmail.com>
2024-12-02 17:10:50 +0200

    Add app configuration related to MQTT
    Initialize MQTT client

--- application/app.py
+++ application/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_mqtt import Mqtt
 
 from application.config import Config
 
@@ -6,3 +7,6 @@ from application.config import Config
 # Initialize the Flask app and load settings from Config
 app = Flask(__name__)
 app.config.from_object(Config)
+
+# Initialize Flask-MQTT client
+mqtt_client = Mqtt(app)
--- application/config.py
+++ application/config.py
@@ -8,3 +8,12 @@ load_dotenv()
 class Config:
     # Flask Settings
     SECRET_KEY = os.getenv("SECRET_KEY")
+
+    # Settings for the MQTT Client
+    MQTT_BROKER_URL = os.getenv("MQTT_BROKER_URL")
+    MQTT_BROKER_PORT = int(os.getenv("MQTT_BROKER_PORT", 1883))
+    MQTT_KEEPALIVE = int(os.getenv("MQTT_KEEPALIVE", 5))
+    MQTT_CLIENT_ID = os.getenv("MQTT_CLIENT_ID", "default_client_id")
+    MQTT_USERNAME = os.getenv("MQTT_USERNAME", '')
+    MQTT_PASSWORD = os.getenv("MQTT_PASSWORD", '')
+    MQTT_TLS_ENABLED = os.getenv("MQTT_TLS_ENABLED", "False").lower() == "true"
