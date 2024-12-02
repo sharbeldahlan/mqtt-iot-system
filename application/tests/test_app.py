f573a6b608e56831e86542d6c849bebf2f62862c
sharbeldahlan <sharbel.dahlan@gmail.com>
2024-12-02 17:20:23 +0200

    Add application/tests and test_app module
    Add new test path to pytest.ini
    Add simple unit test for the Flask app

---
+++ application/tests/test_app.py
@@ -0,0 +1,7 @@
+from application.app import app
+
+
+def test_flask_app_initialization():
+    """Test if Flask app initializes correctly."""
+    assert app is not None
+    assert app.config["MQTT_BROKER_URL"] is not None
